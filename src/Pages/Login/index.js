523e1bdcf159045017af6d8737651cec0675d638
LeeNhaHie <lenhathieu@LeeNhaHies-MacBook-Pro.local>
2020-06-25 23:13:45 +0700

    upload

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,15 +1,31 @@
-import React, {useState} from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
-import {FormGroup, FormLabel,Button, Input} from '@material-ui/core'
+import {
+  FormGroup,
+  Button,
+  Grid,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  FormControl,
+} from "@material-ui/core";
+import { Form, FastField, Formik } from "formik";
 import axios from "axios";
 
-import {URL} from '../../Connect'
+import InputTextField from "../../Components/custom-fields/InputTextField";
 
-function Login(){
-  const [name,setName]= useState()
-  const [password,setPassword] = useState()
-  
-  let history = useHistory()
+import { URL } from "../../Connect";
+
+import loginLogo from "../../Assets/Images/loginLogo.png";
+import "./Login.scss";
+
+function Login() {
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  let history = useHistory();
 
   const onLogin = (name, password) => {
     axios
@@ -21,7 +37,7 @@ function Login(){
             name.slice(0, 3) === "bep"
               ? { from: { pathname: `/${name.slice(4)}/Kitchen` } }
               : { from: { pathname: `/${name.slice(4)}/Dashboard` } };
-          history.replace(from)
+          history.replace(from);
         }
       })
       .catch((err) => {
@@ -30,20 +46,52 @@ function Login(){
   };
 
   return (
-    <form>
-      <FormGroup>
-        <FormLabel>Email address</FormLabel>
-        <Input placeholder="Enter email" value={name} onChange={(event)=>setName(event.target.value)} />
-      </FormGroup>
-    
-      <FormGroup>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" placeholder="Password"value={password} onChange={(event)=>setPassword(event.target.value)}  />
-      </FormGroup>
-      <Button onClick={()=>onLogin(name,password)}>
-        Submit
-      </Button>
-    </form>
+    <Grid className="login-container">
+      <Dialog open={true} style={{borderRadius: 20, padding:10}}>
+        <DialogTitle>
+          <img src={loginLogo} alt="logo" />
+        </DialogTitle>
+        <DialogContent>
+          <Formik
+            initialValues={initialValues}
+            enableReinitialize
+            onSubmit={(value) => onLogin(value.username, value.password)}
+          >
+            {() => {
+              return (
+                <div>
+                  <Form style={{ display: "flex", flexDirection: "column" }}>
+                    <FormGroup className="input-container">
+                      <FastField
+                        name="username"
+                        component={InputTextField}
+                        label="Tên Đăng Nhập"
+                        type="text"
+                      />
+
+                      <FastField
+                        name="password"
+                        component={InputTextField}
+                        label="Mật Khẩu"
+                        type="password"
+                      />
+                    </FormGroup>
+                    <FormControl style={{marginTop:20}}>
+                      <Button
+                        type="submit"
+                        style={{ backgroundColor: "#00b551", color: "white" }}
+                      >
+                        Đăng Nhập
+                      </Button>
+                    </FormControl>
+                  </Form>
+                </div>
+              );
+            }}
+          </Formik>
+        </DialogContent>
+      </Dialog>
+    </Grid>
   );
 }
 
